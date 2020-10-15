78643e7a13a45d9f8fb8447bab4879af1e38640e
Travis Swiers <tswiers85@gmail.com>
2020-10-15 07:26:02 -0600

    test: init

---
+++ tests/ExampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /** @test */
+    public function truth_is_truth()
+    {
+        $this->assertTrue(true);
+    }
+}
